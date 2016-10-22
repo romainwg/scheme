2afd1d1fecbbd607e746b8a9836622f105c93a45
Jérémy Hraman <President@MacBook-Pro-de-Jeremy.local>
2016-10-22 13:24:03 +0200

    eval début

--- scheme/src/eval.c
+++ scheme/src/eval.c
@@ -10,22 +10,78 @@
 
 #include "eval.h"
 
+object eval_define( object input ) {
+    
+    object o_eval=NULL;
+    
+    return o_eval;
+}
+
+object eval_set( object input ) {
+    
+    object o_eval=NULL;
+    
+    return o_eval;
+}
+
+object eval_if( object input ) {
+    
+    object o_eval=NULL;
+    
+    return o_eval;
+}
+
+object eval_and( object input ) {
+    
+    object o_eval=NULL;
+    
+    return o_eval;
+}
+
+object eval_or( object input ) {
+    
+    object o_eval=NULL;
+    
+    return o_eval;
+}
+
+
+/* FONCTION D'EVALUATION */
 
 object sfs_eval( object input ) {
     
     object eval_car = car(input);
+    object o_eval=NULL;
     
-    if ( is_symbol(eval_car) ) {
-        string symbol;
-        strcpy( symbol, eval_car->this.symbol );
+    if ( is_pair(input) ) {
         
-        
-        = eval ;
-        
-        return;
+        if ( is_symbol(eval_car) ) {
+            string function;
+            strcpy( function, eval_car->this.symbol );
+            
+            switch (symbol) {
+                case 'define':
+                    o_eval = eval_define(input);
+                    break;
+                    
+                case 'set!':
+                    o_eval = eval_set(input);
+                    break;
+                    
+                case 'if':
+                    o_eval = eval_if(input);
+                    break;
                 
+                case 'or':
+                    o_eval = eval_or(input);
+                    break;
+                    
+                case 'and':
+                    o_eval = eval_and(input);
+                    break;
+            }
         }
     }
-
+    
     return o_eval;
 }
