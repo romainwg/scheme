3355e7ba53af82f5351af3d223e12a5bbe8cf10c
romainwg <romain.wg@live.fr>
2016-10-25 07:43:16 +0200

    Add some test file - quote

---
+++ scheme/tests_step2/01_quote.scm
@@ -0,0 +1,13 @@
+; TEST_RETURN_CODE=PASS
+; TEST_COMMENT=Test 1 quote
+
+(quote 3)
+(quote "hi")
+(quote a)
+(quote (+ 3 4))
+(quote (a b c))
+(quote (define x 25))
+(quote (lambda (x) (+ x 3)))
+
+;;; Make sure you put a return at
+;;; the end of the input file
