9bdd2ed7a42b5f0a863725f0ec22c3151674be8f
jeremow <jeremy.hraman@laposte.net>
2016-10-25 13:28:33 +0200

    Add files via upload

--- scheme/tests_step2/06_if.scm
+++ scheme/tests_step2/06_if.scm
@@ -1,8 +1,12 @@
 ; TEST_RETURN_CODE=PASS
-; TEST_COMMENT=Test 1 if : simple
+; TEST_COMMENT=Test 05 if
 
-
-
+(if (< 3 4) "3 inférieur à 4")
+(define x 4)
+(if (> x 0) (- x (* 2 x)) x)
+(if (> x 0) (- x) x)
+(if (> x 4) x)
+(if (> x 3) (+ x (- (/ 5 5) (* 3 2))) (- x (- (/ 5 5) (* 3 2))))
 
 
 ;;; Make sure you put a return at
--- scheme/tests_step2/07_and.scm
+++ scheme/tests_step2/07_and.scm
@@ -1,9 +1,20 @@
 ; TEST_RETURN_CODE=PASS
-; TEST_COMMENT=Test 1 and : simple
+; TEST_COMMENT=Test 1 quote
 
-
-
-
+(quote (quote 3))
+(quote (quote "hi"))
+(quote (quote a))
+(quote (quote (+ 3 4)))
+(quote (quote (a b c)))
+(quote (quote (define x 25)))
+(quote (quote (lambda (x) (+ x 3))))
+''3
+''"hi"
+''a
+''(+ 3 4)
+''(a b c)
+''(define x 25)
+''(lambda (x) (+ x 3))
 
 ;;; Make sure you put a return at
 ;;; the end of the input file
